bc2d520268d2de0a28cdb101ddc348c7b7f5418e
ivanov7as <96878229+ivanov7as@users.noreply.github.com>
2023-07-23 19:57:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 num = int(input('Введите число: '))
 
 h = []
-
+hh = 16
 while num > 0:
-    if num % 16 <= 9:
+    if num % hh <= 9:
         h.append(num % 16)
-        num //= 16
-    if num % 16 == 10:
+        num //= hh
+    if num % hh == 10:
         h.append("A")
-        num //= 16
-    if num % 16 == 11:
+        num //= hh
+    if num % hh == 11:
         h.append("B")
-        num //= 16
-    if num % 16 == 12:
+        num //= hh
+    if num % hh == 12:
         h.append("C")
-        num //= 16
-    if num % 16 == 13:
+        num //= hh
+    if num % hh == 13:
         h.append("D")
-        num //= 16
-    if num % 16 == 14:
+        num //= hh
+    if num % hh == 14:
         h.append("E")
-        num //= 16
-    if num % 16 == 15:
+        num //= hh
+    if num % hh == 15:
         h.append("F")
-        num //= 16
+        num //= hh
 h.reverse()
 
 for i in h:
